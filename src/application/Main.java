f1c40a7c9fe812963b5114a3ad2802965f910335
Alexandre Abdu El-moor <alexandre.elmoor@gmail.com>
2022-02-08 17:36:13 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,6 +11,9 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -19,7 +22,7 @@ public class Main extends Application {
 			
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -27,6 +30,11 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 
 	public static void main(String[] args) {
 		launch(args);
